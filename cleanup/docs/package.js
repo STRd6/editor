7ba8f1fa9a6a69efdc77b4ca57a26a1663a6e4b8
Daniel X Moore <yahivin@gmail.com>
2014-11-04 16:01:10 -0800

    :octocat::u6e80: Updated in browser at strd6.github.io/editor

--- cleanup/docs/package.js
+++ cleanup/docs/package.js
@@ -192,7 +192,7 @@
     },
     "pixie.cson": {
       "path": "pixie.cson",
-      "content": "version: \"0.3.0\"\nentryPoint: \"main\"\nwidth: 960\nheight: 800\nremoteDependencies: [\n  \"https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js\"\n  \"https://cdnjs.cloudflare.com/ajax/libs/coffee-script/1.7.1/coffee-script.min.js\"\n  \"https://code.jquery.com/jquery-1.11.0.min.js\"\n]\ndependencies:\n  analytics: \"distri/google-analytics:v0.1.0\"\n  base64: \"distri/base64:v0.9.0\"\n  builder: \"distri/builder:v0.4.2\"\n  chrapps: \"distri/chrapps:v0.1.2\"\n  cornerstone: \"distri/cornerstone:v0.2.6\"\n  cson: \"distri/cson:v0.1.0\"\n  filetree: \"STRd6/filetree:v0.3.2\"\n  github: \"distri/github:v0.4.4\"\n  issues: \"distri/issues:v0.2.3\"\n  md: \"distri/md:v0.4.1\"\n  notifications: \"distri/notifications:v0.3.3\"\n  observable: \"distri/observable:v0.3.1\"\n  hygiene: \"STRd6/hygiene:v0.2.0\"\n  runtime: \"distri/runtime:v0.3.0\"\n  packager: \"distri/packager:v0.5.9-pre.0\"\n  runner: \"distri/runner:v0.2.5\"\n  tests: \"distri/tests:v0.1.1\"\n  \"value-widget\": \"distri/value-widget:v0.1.2\"\n",
+      "content": "version: \"0.3.0\"\nentryPoint: \"main\"\nwidth: 960\nheight: 800\nremoteDependencies: [\n  \"https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js\"\n  \"https://cdnjs.cloudflare.com/ajax/libs/coffee-script/1.7.1/coffee-script.min.js\"\n  \"https://code.jquery.com/jquery-1.11.0.min.js\"\n]\ndependencies:\n  analytics: \"distri/google-analytics:v0.1.0\"\n  base64: \"distri/base64:v0.9.0\"\n  builder: \"distri/builder:v0.4.2\"\n  chrapps: \"distri/chrapps:v0.1.2\"\n  cornerstone: \"distri/cornerstone:v0.2.6\"\n  cson: \"distri/cson:v0.1.0\"\n  filetree: \"STRd6/filetree:v0.3.2\"\n  github: \"distri/github:v0.4.4\"\n  issues: \"distri/issues:v0.2.3\"\n  md: \"distri/md:v0.4.1\"\n  notifications: \"distri/notifications:v0.3.3\"\n  observable: \"distri/observable:v0.3.1\"\n  hygiene: \"STRd6/hygiene:v0.2.0\"\n  runtime: \"distri/runtime:v0.3.0\"\n  packager: \"distri/packager:v0.5.10\"\n  runner: \"distri/runner:v0.2.5\"\n  tests: \"distri/tests:v0.1.1\"\n  \"value-widget\": \"distri/value-widget:v0.1.2\"\n",
       "mode": "100644",
       "type": "blob"
     },
@@ -293,7 +293,7 @@
     },
     "pixie": {
       "path": "pixie",
-      "content": "module.exports = {\"version\":\"0.3.0\",\"entryPoint\":\"main\",\"width\":960,\"height\":800,\"remoteDependencies\":[\"https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js\",\"https://cdnjs.cloudflare.com/ajax/libs/coffee-script/1.7.1/coffee-script.min.js\",\"https://code.jquery.com/jquery-1.11.0.min.js\"],\"dependencies\":{\"analytics\":\"distri/google-analytics:v0.1.0\",\"base64\":\"distri/base64:v0.9.0\",\"builder\":\"distri/builder:v0.4.2\",\"chrapps\":\"distri/chrapps:v0.1.2\",\"cornerstone\":\"distri/cornerstone:v0.2.6\",\"cson\":\"distri/cson:v0.1.0\",\"filetree\":\"STRd6/filetree:v0.3.2\",\"github\":\"distri/github:v0.4.4\",\"issues\":\"distri/issues:v0.2.3\",\"md\":\"distri/md:v0.4.1\",\"notifications\":\"distri/notifications:v0.3.3\",\"observable\":\"distri/observable:v0.3.1\",\"hygiene\":\"STRd6/hygiene:v0.2.0\",\"runtime\":\"distri/runtime:v0.3.0\",\"packager\":\"distri/packager:v0.5.9-pre.0\",\"runner\":\"distri/runner:v0.2.5\",\"tests\":\"distri/tests:v0.1.1\",\"value-widget\":\"distri/value-widget:v0.1.2\"}};",
+      "content": "module.exports = {\"version\":\"0.3.0\",\"entryPoint\":\"main\",\"width\":960,\"height\":800,\"remoteDependencies\":[\"https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js\",\"https://cdnjs.cloudflare.com/ajax/libs/coffee-script/1.7.1/coffee-script.min.js\",\"https://code.jquery.com/jquery-1.11.0.min.js\"],\"dependencies\":{\"analytics\":\"distri/google-analytics:v0.1.0\",\"base64\":\"distri/base64:v0.9.0\",\"builder\":\"distri/builder:v0.4.2\",\"chrapps\":\"distri/chrapps:v0.1.2\",\"cornerstone\":\"distri/cornerstone:v0.2.6\",\"cson\":\"distri/cson:v0.1.0\",\"filetree\":\"STRd6/filetree:v0.3.2\",\"github\":\"distri/github:v0.4.4\",\"issues\":\"distri/issues:v0.2.3\",\"md\":\"distri/md:v0.4.1\",\"notifications\":\"distri/notifications:v0.3.3\",\"observable\":\"distri/observable:v0.3.1\",\"hygiene\":\"STRd6/hygiene:v0.2.0\",\"runtime\":\"distri/runtime:v0.3.0\",\"packager\":\"distri/packager:v0.5.10\",\"runner\":\"distri/runner:v0.2.5\",\"tests\":\"distri/tests:v0.1.1\",\"value-widget\":\"distri/value-widget:v0.1.2\"}};",
       "type": "blob"
     },
     "runners": {
@@ -7155,7 +7155,7 @@
         },
         "pixie.cson": {
           "path": "pixie.cson",
-          "content": "version: \"0.5.9-pre.0\"\nentryPoint: \"packager\"\nremoteDependencies: [\n  \"https://code.jquery.com/jquery-1.11.0.min.js\"\n]\ndependencies:\n  require: \"distri/require:v0.4.2\"\n  memoize_promise: \"distri/memoize_promise:v0.1.0\"\n",
+          "content": "version: \"0.5.10\"\nentryPoint: \"packager\"\nremoteDependencies: [\n  \"https://code.jquery.com/jquery-1.11.0.min.js\"\n]\ndependencies:\n  require: \"distri/require:v0.4.2\"\n  memoize_promise: \"distri/memoize_promise:v0.1.0\"\n",
           "mode": "100644",
           "type": "blob"
         },
@@ -7185,7 +7185,7 @@
         },
         "pixie": {
           "path": "pixie",
-          "content": "module.exports = {\"version\":\"0.5.9-pre.0\",\"entryPoint\":\"packager\",\"remoteDependencies\":[\"https://code.jquery.com/jquery-1.11.0.min.js\"],\"dependencies\":{\"require\":\"distri/require:v0.4.2\",\"memoize_promise\":\"distri/memoize_promise:v0.1.0\"}};",
+          "content": "module.exports = {\"version\":\"0.5.10\",\"entryPoint\":\"packager\",\"remoteDependencies\":[\"https://code.jquery.com/jquery-1.11.0.min.js\"],\"dependencies\":{\"require\":\"distri/require:v0.4.2\",\"memoize_promise\":\"distri/memoize_promise:v0.1.0\"}};",
           "type": "blob"
         },
         "test/deferred": {
@@ -7202,13 +7202,13 @@
       "progenitor": {
         "url": "http://www.danielx.net/editor/"
       },
-      "version": "0.5.9-pre.0",
+      "version": "0.5.10",
       "entryPoint": "packager",
       "remoteDependencies": [
         "https://code.jquery.com/jquery-1.11.0.min.js"
       ],
       "repository": {
-        "branch": "v0.5.9-pre.0",
+        "branch": "v0.5.10",
         "default_branch": "master",
         "full_name": "distri/packager",
         "homepage": null,
